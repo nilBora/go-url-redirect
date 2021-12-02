62e69e8ca5f5aad4d8a0239738e178534833ea93
Nil Borodulia <nborodulia@gmail.com>
2021-12-02 15:18:58 +0200

    add dbContaainer

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	//"github.com/joho/godotenv"
 	_"github.com/lib/pq"
 )
 
@@ -19,6 +18,10 @@ type LinkRedirect struct {
 	Host string `db:"host"`
 	Link string `db:"link"`
 	Code string `db:"code"`
+}
+
+type DB struct {
+	connection *sql.DB
 }
 
 type RedirectRepository struct {
@@ -43,15 +46,15 @@ var db *sql.DB
 
 func main() {
     utils.LoadEnv()
-	initDB()
-	defer db.Close()
+	dbContainer := initDB()
+	defer dbContainer.connection.Close()
 
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-    e.GET("/:code", doRedirect)
+    e.GET("/:code", doRedirect(dbContainer))
 
 	httpPort := getHttpPort()
 
@@ -66,12 +69,13 @@ func getHttpPort() string {
     return httpPort
 }
 
-func doRedirect(c echo.Context) error {
+func doRedirect(dbContainer DB) echo.HandlerFunc {
+  return func(c echo.Context) error {
     code := c.Param("code")
     var link LinkRedirect
 
     var redirectRepository RedirectRepository
-    redirectRepository.connection = db
+    redirectRepository.connection = dbContainer.connection
     link, queryErr := redirectRepository.getByCode(code)
 
     var result string
@@ -88,12 +92,37 @@ func doRedirect(c echo.Context) error {
 
 
     return c.HTML(http.StatusOK, "Url: "+code+" Res: "+result)
-    //return c.Redirect(301, link.Link)
-    //return nil
-    //
+  }
 }
 
-func initDB() {
+// func doRedirect(c echo.Context, dbContainer DB) error {
+//     code := c.Param("code")
+//     var link LinkRedirect
+//
+//     var redirectRepository RedirectRepository
+//     redirectRepository.connection = dbContainer.connection
+//     link, queryErr := redirectRepository.getByCode(code)
+//
+//     var result string
+//
+//     switch queryErr {
+//         case sql.ErrNoRows:
+//           result = "No rows were returned!"
+//           return c.HTML(http.StatusOK, "No rows were returned!")
+//         case nil:
+//           result = link.Link
+//         default:
+//           panic(queryErr)
+//     }
+//
+//
+//     return c.HTML(http.StatusOK, "Url: "+code+" Res: "+result)
+//     //return c.Redirect(301, link.Link)
+//     //return nil
+//     //
+// }
+
+func initDB() DB {
     psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
                  "password=%s dbname=%s sslmode=disable",
                  os.Getenv("APP_POSTGRESQL_HOST"),
@@ -103,20 +132,16 @@ func initDB() {
                  os.Getenv("POSTGRESQL_DB"))
 
     var err error
+    var dbContainer DB
 
-    db, err = sql.Open("postgres", psqlInfo)
+    dbContainer.connection, err = sql.Open("postgres", psqlInfo)
     if err != nil {
         panic(err)
     }
 
-    err = db.Ping()
+    err = dbContainer.connection.Ping()
     if err != nil {
         panic(err)
     }
+    return dbContainer
 }
-
-// func loadEnv() {
-//     if err := godotenv.Load(); err != nil {
-//         panic("No .env file found")
-//     }
-// }
