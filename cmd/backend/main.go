3ee44be5182d27a9f2b550a38f9abd8485cf98c6
Nil Borodulia <nborodulia@gmail.com>
2021-12-16 16:42:44 +0200

    remove old code

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -15,27 +15,6 @@ import (
 	_"github.com/lib/pq"
 )
 
-type LinkRedirect struct {
-	ID   int    `db:"id"`
-	Host string `db:"host"`
-	Link string `db:"link"`
-	Code string `db:"code"`
-}
-
-type RedirectRepository struct {
-	connection *sql.DB
-}
-
-func (repository *RedirectRepository) getByCode(code string) (LinkRedirect, error) {
-    var link LinkRedirect
-
-    sqlStatement := `SELECT id, host, link, code FROM redirects WHERE code = $1`
-    row := repository.connection.QueryRow(sqlStatement, code)
-    queryErr := row.Scan(&link.ID, &link.Host, &link.Link, &link.Code)
-
-    return link, queryErr
-}
-
 func main() {
     utils.LoadEnv()
 	dbContainer := dbConnector.Init()
